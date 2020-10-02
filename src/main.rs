d6f30ea469c3cabdfcec0ff9e2d13edf6e5646ff
joeclark-phd <joe@joeclark.net>
2020-10-02 17:11:47 -0400

    a basic letter-counting program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut letter_counts: HashMap<char,i32> = HashMap::new();
+
+    let input_string = "Hello, world!";
+    let char_vec: Vec<char> = input_string.to_lowercase().chars().collect();
+    for c in char_vec {
+        if let Some(x) = letter_counts.get_mut(&c) {
+            *x = *x + 1;
+        } else {
+            letter_counts.insert(c,1);
+        }
+    }
+    println!("{:?}",letter_counts);
 }
