c057a32166aba0c207b81076d94f60992f799a03
joeclark-phd <joe@joeclark.net>
2020-10-05 15:33:12 -0400

    keys to main hashmap are now vectors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut letter_counts: HashMap<char,i32> = HashMap::new();
+    let mut letter_counts: HashMap<Vec<char>,i32> = HashMap::new();
 
     let input_string = "Hello, world!";
     let char_vec: Vec<char> = input_string.to_lowercase().chars().collect();
     for c in char_vec {
-        if let Some(x) = letter_counts.get_mut(&c) {
+        if let Some(x) = letter_counts.get_mut(&vec!(c)) {
             *x = *x + 1;
         } else {
-            letter_counts.insert(c,1);
+            letter_counts.insert(vec!(c),1);
         }
     }
     println!("{:?}",letter_counts);
