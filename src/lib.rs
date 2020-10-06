7415807d10c294f34c91beba3f7edfb1d0c782de
joeclark-phd <joe@joeclark.net>
2020-10-06 14:39:31 -0400

    we now build multi-order character models

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 use std::collections::{HashMap, HashSet};
+use std::cmp::max;
 
 pub struct MarkovModel {
-    pub frequencies: HashMap<Vec<char>,HashMap<Vec<char>,i32>>,
+    pub frequencies: HashMap<Vec<char>,HashMap<char,i32>>,
     pub alphabet: HashSet<char>,
+    order: i32
 }
 impl MarkovModel {
+
+    const DEFAULT_ORDER: i32 = 3;
+
     pub fn new() -> MarkovModel {
         MarkovModel{
             frequencies: HashMap::new(),
-            alphabet: HashSet::new()
+            alphabet: HashSet::new(),
+            order: MarkovModel::DEFAULT_ORDER,
         }
     }
+
+    // TODO: write method to train on a sequence/stream of Strings e.g. from a file
+
+    // TODO: write tests
     pub fn add_sequence(&mut self, sequence: &str) -> Result<(), &'static str> {
         if sequence.len() < 2 { return Err("sequence was too short, must contain at least two characters") };
 
@@ -19,12 +29,14 @@ impl MarkovModel {
         for i in (1..char_vec.len()).rev() {
             // Build a running set of all known characters while we're at it
             self.alphabet.insert(char_vec[i]);
-            // For the character at (i-1), record that character at (i) occurred following it.
-            // TODO: build multi-order models
-            *self.frequencies.entry(vec!(char_vec[i-1])).or_insert(HashMap::new()).entry(vec!(char_vec[i])).or_insert(0) += 1;
-            println!("{}",i);
+            // For the sequences preceding character (i), record that character (i) was observed following them.
+            // IE if the char_vec is ['R','U','S','T'] and this is a 3rd-order model, then for the three models ['S'], ['U','S'], and ['R','U','S'] we record that ['T'] is a known follower.
+            for j in (max(0,i as i32 - self.order) as usize)..i {
+                *self.frequencies.entry(Vec::from(&char_vec[j..i])).or_insert(HashMap::new()).entry(char_vec[i]).or_insert(0) += 1;
+            }
         }
         self.alphabet.insert(char_vec[0]); // previous loop stops before index 0
         Ok(())
     }
+
 }
