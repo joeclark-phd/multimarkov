4dcbb46ba80fd5bb33be69b70435910fe903311e
joeclark-phd <joe@joeclark.net>
2020-10-14 23:43:49 -0400

    test name generation with real training data

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use multimarkov::MultiMarkovModel;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 fn main() {
     let mut model = MultiMarkovModel::<char>::new();
 
-    let input_vec = vec![
-        vec!['a','c','e'],
-        vec!['f','o','o','b','a','r'],
-        vec!['b','a','z'],
-    ];
-    model.add_sequences(input_vec.into_iter()).unwrap_or_else(|err| println!("Problem training model: {}", err));
+    let file = File::open("resources/romans.txt").unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines().map(|l| l.unwrap().to_lowercase() ).map(|l| l.chars().collect::<Vec<_>>()).map(|mut v| { v.insert(0, '#'); v.push('#'); v });
+    model.add_sequences(lines).unwrap_or_else(|err| println!("Problem training model: {}", err));
     model.add_priors(MultiMarkovModel::<char>::DEFAULT_PRIOR);
-    println!("frequencies: {:?}",model.frequencies);
-    println!("known states: {:?}",model.known_states);
-    println!("random next for 'abba': {:?}", model.random_next(&vec!['a','b','b','a']) );
 
+    for _i in 0..10 {
+        // generate a roman-sounding name
+        let mut name = vec!['#']; // the beginning-of-word and end-of-word character
+        name.push(model.random_next(&name).unwrap());
+        while !name.ends_with(&*vec!['#']) {
+            name.push(model.random_next(&name).unwrap());
+        }
+        name.pop();
+        name.remove(0);
+        let stringname = name.iter().collect::<String>();
+        println!("{}", stringname);
+    }
 }
 
 
